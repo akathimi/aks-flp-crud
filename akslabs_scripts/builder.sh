93784f818871145233e7faf05a160fb48d03d215
akram <akram.kathimi@gmail.com>
2022-06-13 14:05:18 +0300

    refactoring

--- akslabs_scripts/builder.sh
+++ akslabs_scripts/builder.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 SAP=$1
-out_file="$SAP.sh"
+out_dir="labs_sh"
+out_file="$out_dir/$SAP.sh"
 
+mkdir -p $out_dir
 labs=$(ls -x "$SAP")
 sed -e "s/labs=.*/labs=(${labs[*]})/" main.sh > tmp_file
 
--- shc_script_converter.sh
+++ shc_script_converter.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 ## Script to convert the base lab scripts to binaries
-
+shell_path=akslabs_scripts/labs_sh
 SHC_STATUS=$(which shc > /dev/null; echo $?)
-if [ $SHC_STATUS -ne 0 ]
+if [ "$SHC_STATUS" -ne 0 ]
 then
     echo -e "\nError: missing shc binary...\n"
     exit 4
 fi
 
-AKSLABS_SCRIPTS="$(ls ./akslabs_scripts/)"
+AKSLABS_SCRIPTS="$(ls $shell_path)"
 if [ -z "$AKSLABS_SCRIPTS" ]
 then
     echo -e "Error: missing akslabs scripts...\n"
@@ -19,13 +19,13 @@ fi
 function convert_to_binary() {
     SCRIPT_NAME="$1"
     BINARY_NAME="$(echo "$SCRIPT_NAME" | sed 's/.sh//')"
-    shc -f ./akslabs_scripts/${SCRIPT_NAME} -r -o ./akslabs_binaries/${BINARY_NAME}
-    rm -f ./akslabs_scripts/${SCRIPT_NAME}.x.c > /dev/null 2>&1
+    shc -f $shell_path/"${SCRIPT_NAME}" -r -o ./akslabs_binaries/"${BINARY_NAME}"
+    rm -f $shell_path/"${SCRIPT_NAME}".x.c > /dev/null 2>&1
 }
 
 for FILE in $(echo "$AKSLABS_SCRIPTS")
 do
-    convert_to_binary $FILE
+    convert_to_binary "$FILE"
 done
 
 exit 0
